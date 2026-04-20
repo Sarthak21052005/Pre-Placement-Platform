00feeb87fc05b8c65c0a5e9d3f26c8e7604ea67b
Sarthak <thapliyalsarthak38@gmail.com>
2026-04-21 04:10:04 +0530

    Fix the attempt table

--- backend/routes/attempt_routes.py
+++ backend/routes/attempt_routes.py
@@ -6,6 +6,8 @@ from database.database import get_db
 from schemas.attempt_schema import AttemptCreate
 from CRUD.attempts_crud import create_attempt, get_user_attempts, get_user_stats, get_all_attempts
 from bson import ObjectId
+import json
+
 
 router = APIRouter()
 
@@ -18,28 +20,24 @@ def add_attempt(attempt: AttemptCreate, db: Session = Depends(get_db)):
 def get_all_attempts_route(db : Session = Depends(get_db)):
     return get_all_attempts(db)
 
-
-# GET USER ATTEMPTS
+#GET ALL USER ATTEMPTS
 @router.get("/attempts/user/{user_id}")
 def get_user_attempts_route(user_id: int, db: Session = Depends(get_db)):
-    attempts = db.query(Attempt).filter(
-        Attempt.user_id == user_id,
-        Attempt.status == "solved"
-    ).all()
+    attempts = get_user_attempts(db, user_id)
 
     result = []
 
     for a in attempts:
-        question = questions_collection.find_one(
-            {"_id": ObjectId(a.question_id)},
-            {"title": 1, "company": 1, "difficulty": 1}
-        )
+        try:
+            companies = json.loads(a.company_names)
+        except:
+            companies = ["Unknown"]
 
         result.append({
             "id": str(a.id),
             "question_id": a.question_id,
-            "title": question.get("title") if question else "Unknown",
-            "company": question.get("company", ["Unknown"])[0] if question else "Unknown",
+            "title": a.question_name,
+            "companies": companies,
             "difficulty": a.difficulty,
             "date": a.created_at
         })
@@ -55,9 +53,20 @@ def get_stats(user_id: int, db: Session = Depends(get_db)):
 #HEATMAP DATA
 @router.get("/attempts/heatmap/{user_id}")
 def get_heatmap_data(user_id: int, db: Session = Depends(get_db)):
-    attempts = db.query(Attempt).filter(Attempt.user_id == user_id , Attempt.status == "solved").all()
+    attempts = get_user_attempts(db, user_id)
     heatmap_data = {}
     for a in attempts:
         date_str = a.created_at.date().isoformat() 
         heatmap_data[date_str] = heatmap_data.get(date_str, 0) + 1
     return heatmap_data
+
+#RUN 
+@router.post("/run")
+def run_code(data : dict):
+    code = data.get("code")
+    language = data.get("language")
+    input_data = data.get("input")
+
+    return{
+            "output": f"Input:\n{input_data}\n\nOutput:\nDemo Result"
+    }
